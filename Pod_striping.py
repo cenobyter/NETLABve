70f6152c354910cc5b0a16b90832bc318987bb8d
cenobyter <dckhovey@me.com>
2019-01-17 11:16:32 -0800

    Updated to remove tracker and use stepping

--- Pod_striping.py
+++ Pod_striping.py
@@ -2,7 +2,7 @@ import datetime, json
 from netlab.client import Client
 
 def pod_striping(pod_base=None,parent_pod=None,pod_numbers=32,pod_name_base=None,filename="./pc_clone_spec.json",
-        set1="pc_clone_spec1",set2="pc_clone_spec2",set3="pc_clone_spec3"):
+        set1="pc_clone_spec1",set2="pc_clone_spec2",set3="pc_clone_spec3",replacement=False):
     #argument usage/definitions
     #pod_base is used to decipher which pod # for the specific set
     #parent_pod is the pod to be cloned
@@ -10,6 +10,7 @@ def pod_striping(pod_base=None,parent_pod=None,pod_numbers=32,pod_name_base=None
     #pod numbers will be added in the cloning step from the range utilizing the pod_base
     #pod_name_base is the string that is re-used by all pods
     #example pod_name_base="Cyber_Patriots_State_Silver/MiddleSchool_Pod-%02d"
+    #replacement is used to define if it is replacing an existing pod set, if true removes old pods
 
 
     #begin clone specs easier to edit the clone specs settings below than here
@@ -27,76 +28,40 @@ def pod_striping(pod_base=None,parent_pod=None,pod_numbers=32,pod_name_base=None
     #pod_set is the range of pod destinations to be cloned to
     pod_set=range(pod_base+1,pod_base+(pod_numbers+1))
 
-    tracker=1
-
     tapi = Client()
 
-    #Tack Pods Offline
-    for pod in pod_set:
-        if tracker==4:
-            tracker=1
-        print("taking pod "+str(pod)+" offline")
-        tapi.pod_state_change(pod_id=pod,state='OFFLINE')
-        tracker+=1
+    if replacement:
+        #Tack Pods Offline
+        for pod in pod_set:
+            print("taking pod "+str(pod)+" offline")
+            tapi.pod_state_change(pod_id=pod,state='OFFLINE')
+        #Remove Pods
 
-    tracker=1
-
-    #Remove Pods
-
-    for pod in pod_set:
-        if tracker==4:
-            tracker=1
-        if tracker==1:
-            print("removing pod "+str(pod))
-            tapi.pod_remove_task(pod_id=pod,remove_vms='DISK')
-        if tracker==2:
-            print("removing pod "+str(pod))
-            tapi.pod_remove_task(pod_id=pod,remove_vms='DISK')
-        if tracker==3:
-            print("removing pod "+str(pod))
-            tapi.pod_remove_task(pod_id=pod,remove_vms='DISK')
-
-        tracker+=1
-
-    tracker=1
+        for pod in pod_set:
+                print("removing pod "+str(pod))
+                tapi.pod_remove_task(pod_id=pod,remove_vms='DISK')
 
     #Creating new pods
 
 
-    for pod in pod_set:
+    for pod in pod_set[::3]:
         print("Creating pod %02d"%(pod-pod_base))
-        if tracker==4:
-            tracker=1
-        if tracker==1:
-            print("cloning to host vh id "+str(pc_clone_specs1['clone_vh_id']))
-            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
-            pc_clone_specs=pc_clone_specs1)
-        if tracker==2:
-            print("cloning to host vh id "+str(pc_clone_specs2['clone_vh_id']))
-            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
-            pc_clone_specs=pc_clone_specs2)
-        if tracker==3:
-            print("cloning to host vh id "+str(pc_clone_specs3['clone_vh_id']))
-            tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
-            pc_clone_specs=pc_clone_specs3)
-        tracker+=1
+        print("cloning to host vh id "+str(pc_clone_specs1['clone_vh_id']))
+        tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
+        pc_clone_specs=pc_clone_specs1)
+    for pod in pod_set[1::3]:   
+        print("cloning to host vh id "+str(pc_clone_specs2['clone_vh_id']))
+        tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
+        pc_clone_specs=pc_clone_specs2)
+    for pod in pod_set[2::3]:
+        print("cloning to host vh id "+str(pc_clone_specs3['clone_vh_id']))
+        tapi.pod_clone_task(source_pod_id=parent_pod,clone_pod_id=pod,clone_pod_name=pod_name_base%(pod-pod_base),
+        pc_clone_specs=pc_clone_specs3)
+
 
 
     #Bringing new pods online
-
-    tracker=1
-
     for pod in pod_set:
-        if tracker==4:
-            tracker=1
-        if tracker==1:
             print("taking pod "+str(pod)+" online")
-            tapi.pod_state_change(pod_id=pod,state='ONLINE')
-        if tracker==2:
-            print("taking pod "+str(pod)+" online")
-            tapi.pod_state_change(pod_id=pod,state='ONLINE')
-        if tracker==3:
-            print("taking pod "+str(pod)+" online")
-            tapi.pod_state_change(pod_id=pod,state='ONLINE')
-        tracker+=1
+
 
