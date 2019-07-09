1b4c4611e804ad882645c3324b1bd42fba0eda43
cenobyter <dckhovey@me.com>
2019-07-09 10:07:02 -0700

    fixed syntax issues with single host offline

--- single_host_offline.py
+++ single_host_offline.py
@@ -12,14 +12,14 @@ tapi = Client()
 #
 def single_host_offline(*,offline_host):
     #import the PCType for use later in a sanity check
-    from netlab.enum import PCType
+    from netlab.enums import PCType
     #for loop to run through all pods on the system
     for pod in tapi.pod_list():
         #assign the pc value to the first pc in the pod
         #this will be used to verify that the pod exists on the server
         pc = tapi.pod_pc_get(pod_id=pod['pod_id'],pl_index=1)
         #check to see that the vm exists and it exists on the specified host
-        if pc['pc_type'] !=PCType.ABSENT and offline_host =pc['vh_name']:
+        if pc['pc_type'] !=PCType.ABSENT and offline_host==pc['vh_name']:
             #take pod offline and provide console feedback on activity
-            print(pc['taking pod '+ str(pod['pod_id']+' offline')])
-            tapi.pod_state_change(pod_id=pod,state='OFFLINE')
+            print('taking pod '+ str(pod['pod_id'])+' offline')
+            tapi.pod_state_change(pod_id=['pod_id'],state='OFFLINE')
