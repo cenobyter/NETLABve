b8cf43b3570e6ce03503fe4816bf1c37a33ecd11
cenobyter <dckhovey@me.com>
2019-07-11 07:41:11 -0700

    fixed indent error

--- student_drop_removal.py
+++ student_drop_removal.py
@@ -1,6 +1,16 @@
-def student_drop_removal(int class, str student_file_location):
+from netlab.client import Client
+from netlab.enums import AccountType
+import datetime
+
+tapi = Client()
+
+def student_drop_removal(Netlab_Class,student_file_location):
     with open(student_file_location) as dropped_students:
         student_name= dropped_student.readline()
-            for student in tapi.class_roster_list(cls_id=class): student_to_drop=student[‘acc_id’] if student[‘acc_full_name’]==student_name else None
-                tapi.class_roster_remove(cls_id=class, acc_id=student_to_drop) if student_to_drop!= None else print(“student “+student_to_drop+” not found”)
+        for student in tapi.class_roster_list(cls_id=Netlab_Class):
+            student_to_drop=student[‘acc_id’] if student[‘acc_full_name’]==student_name else None
+            if student_to_drop!= None:
+                tapi.class_roster_remove(cls_id=Netlab_Class, acc_id=student_to_drop)
+            else:
+                    print(“student “+student_to_drop+” not found”)
 
