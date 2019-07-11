af6b4bdfed5a439510cd1e34e21947a50ab47f42
cenobyter <dckhovey@me.com>
2019-07-11 07:43:22 -0700

    fixed ticks to be single quotes

--- student_drop_removal.py
+++ student_drop_removal.py
@@ -1,16 +1,15 @@
 from netlab.client import Client
-from netlab.enums import AccountType
 import datetime
 
 tapi = Client()
 
 def student_drop_removal(Netlab_Class,student_file_location):
     with open(student_file_location) as dropped_students:
-        student_name= dropped_student.readline()
+        student_name= dropped_students.readline()
         for student in tapi.class_roster_list(cls_id=Netlab_Class):
-            student_to_drop=student[‘acc_id’] if student[‘acc_full_name’]==student_name else None
+            student_to_drop=student['acc_id'] if student['acc_full_name']==student_name else None
             if student_to_drop!= None:
                 tapi.class_roster_remove(cls_id=Netlab_Class, acc_id=student_to_drop)
             else:
-                    print(“student “+student_to_drop+” not found”)
+                    print("student "+student_to_drop+" not found")
 
