ec2d07006de0d02c721c64f68cd149cb0d2e195d
cenobyter <dckhovey@me.com>
2019-09-23 10:11:21 -0700

    Fixed Import-VappFolder

---
+++ Import-VappFolder.ps1
@@ -0,0 +1,32 @@
+function Import-VappFolder{
+    Param(
+        [Parameter(Mandatory)]
+        [string]$SourceFolder
+        ,
+        [Parameter(Mandatory)]
+        [string]$Server
+        ,
+        [Parameter(Mandatory)]
+        [string]$Storage
+        ,
+        [Parameter(Mandatory=$false)]
+        [string]$DestinationFolder
+        )
+
+    $vmHost = Get-VMHost -Name $server
+
+    $vmDataStore = Get-Datastore -Name $Storage
+
+    if($DestinationFolder -ne ""){
+        $vmFolder= Get-Folder -Name $DestinationFolder
+    }
+    Get-Childitem -Recurse -filter *.ov* $SourceFolder | Where-Object{$_.name -match ".ov[af]"} | foreach-object {
+        #folder deployment (location) does not seem to work the way I want it to
+        if($DestinationFolder -ne ""){
+            Import-Vapp -Source $_.fullname -Datastore $vmDataStore -VMHost $vmHost -InventoryLocation $vmFolder -whatif
+        }
+        else{
+            Import-Vapp -Source $_.fullname -Datastore $vmDataStore -VMHost $vmHost -whatif
+        }
+    }
+}
